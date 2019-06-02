d112f30622395a403c05e266f803ed9b211ae600
Lukas <lukas.hodan1@gmail.com>
2019-06-02 16:59:11 +0200

    enter

--- server/app.js
+++ server/app.js
@@ -25,6 +25,7 @@ app.use(cookieParser());
 app.use(expressValidator());
 sessionMangementConfig(app, db);
 
+
 app.use(cors());
 app.use('/api', router);
 app.use(express.static(path.resolve(__dirname, '../static')));
