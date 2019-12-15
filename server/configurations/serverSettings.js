0ec632575374d07de45f797ec7b9b52248973881
Lukas <lukas.hodan1@gmail.com>
2019-12-15 18:27:14 +0100

    mongo connection

--- server/configurations/serverSettings.js
+++ server/configurations/serverSettings.js
@@ -1,6 +1,6 @@
 export function getMongoConnection(){
     if (process.env.NODE_ENV === 'production') {
-        return 'mongodb://Needmayer:o4Lh7jJP@ds119508.mlab.com:19508/food_diary';
+        return 'mongodb://Needmayer:o4Lh7jJP@ds261116.mlab.com:61116/food_diary';
     } else {
         return 'mongodb://localhost:27017/food_diary';
     }
